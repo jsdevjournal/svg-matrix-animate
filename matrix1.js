b2bce720d5a93d0cdc95590ced7eb874755f89e9
Krit <suns_stat@hotmail.com>
2023-08-31 13:24:00 +0700

    Refactor functions

--- matrix1.js
+++ matrix1.js
@@ -1,111 +1,169 @@
 
-// Config
-const windowHeight = window.innerHeight;
-const windowWidth = window.innerWidth;
-const color = 'green';
-const words = [
-    "HELLO", "WORLD", "MATRIX", "ANIMATION", "JAVASCRIPT", "SVG"
-]; // Words to animate
-const getRandomNumber = (min, max) => () => Math.floor(Math.random() * (max - min) + min);
-const getAnimationDuration = getRandomNumber(3, 8); // Range of animation durations (in seconds)
-const getFontSize = getRandomNumber(10, 40); // Range of font sizes (in pixels)
-const getX = getRandomNumber(0, windowWidth);
-const getWordIndex = getRandomNumber(0, words.length - 1);
-const getTimeout = getRandomNumber(50, 500);
-let theMatrix = [];
-
-// Create an SVG element
 const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
 svg.setAttribute('width', '100%');
 svg.setAttribute('height', '100%');
 document.getElementById("matrix-container").appendChild(svg);
 
-function getRandomCharacter() {
-    return String.fromCharCode(Math.random() * 94 + 33);
+const WORDS = [
+  "Algorithm",
+  "Artificial",
+  "Augmented",
+  "Automation",
+  "Big Data",
+  "Biometrics",
+  "Blockchain",
+  "Cloud",
+  "Coding",
+  "Cryptocurrency",
+  "Cybersecurity",
+  "Database",
+  "Deep Learning",
+  "Digital",
+  "Encryption",
+  "Firmware",
+  "Gadget",
+  "Genome",
+  "Hologram",
+  "Innovation",
+  "Internet",
+  "Machine Learning",
+  "Microchip",
+  "Mobile",
+  "Network",
+  "Open Source",
+  "Operating System",
+  "Quantum",
+  "Robotics",
+  "Software",
+  "Virtual",
+  "Wireless",
+  "3D Printing",
+  "Agile",
+  "Analytics",
+  "Artificial Intelligence",
+  "Authentication",
+  "Automation",
+  "Backend",
+  "Big Data",
+  "Biometrics",
+  "Blockchain",
+  "Cloud Computing",
+  "Code",
+  "Compiler",
+  "Computer",
+  "Cybersecurity",
+  "Data Mining",
+  "Data Science",
+  "Database",
+  "Debugging",
+  "Digital",
+  "E-commerce",
+  "Encryption",
+  "Firmware",
+  "Frontend",
+  "Gadget",
+  "Hacking",
+  "Hardware",
+  "HTML",
+  "Information",
+  "Innovation",
+  "Internet",
+  "IoT",
+  "JavaScript",
+  "Kernel",
+  "Machine Learning",
+  "Malware",
+  "Metadata",
+  "Microchip",
+  "Mobile",
+  "Network",
+  "Operating System",
+  "Password",
+  "Programming",
+  "Quantum",
+  "Responsive",
+  "Robotics",
+  "Scalability",
+  "Security",
+  "Server",
+  "Software",
+  "Technology",
+  "Virtual",
+  "Web Development",
+  "Website",
+  "Wireless",
+];
+
+const DEFAULT_WORD_COUNT = 150;
+const MIN_DURATION = 2;
+const MAX_DURATION = 12;
+const MIN_FONTSIZE = 10;
+const MAX_FONTSIZE = 40;
+const getRandomNumber = (min, max) => () => Math.floor(Math.random() * (max - min) + min);
+const windowHeight = window.innerHeight;
+const windowWidth = window.innerWidth;
+const numRows = 30; // Number of rows in animation
+const getAnimationDuration = getRandomNumber(MIN_DURATION, MAX_DURATION); // Range of animation durations (in seconds)
+const getFontSize = getRandomNumber(MIN_FONTSIZE, MAX_FONTSIZE); // Range of font sizes (in pixels)
+const getX = getRandomNumber(0, windowWidth);
+const getWordIndex = getRandomNumber(0, WORDS.length - 1);
+
+function createElementNS(name) {
+  return document.createElementNS('http://www.w3.org/2000/svg', name);
 }
 
-function shiftArray(arr, val) {
-    return [
-        val,
-        ...arr
-    ].slice(0, -1);
+function createTextElem(x, y, character, fontSize) {
+  const textElement = createElementNS('text');
+  textElement.textContent = character;
+  textElement.setAttribute('x', x);
+  textElement.setAttribute('y', y);
+  textElement.setAttribute('fill', 'green');
+  textElement.setAttribute('fill-opacity', fontSize / MAX_FONTSIZE);
+  textElement.setAttribute('font-size', fontSize);
+  return textElement;
 }
 
-function unmountSvg(arr) {
-    arr.forEach(text => svg.removeChild(text));
+function createAnimationElem(startX, endX, startY, endY) {
+  const animationDuration = getAnimationDuration();
+  const animateElement = createElementNS('animateTransform');
+  animateElement.setAttribute('attributeName', 'transform');
+  animateElement.setAttribute('attributeType', 'XML');
+  animateElement.setAttribute('type', 'translate');
+  animateElement.setAttribute('values', `${startX} ${startY}; ${endX} ${endY}`);
+  animateElement.setAttribute('dur', `${animationDuration}s`);
+  animateElement.setAttribute('repeatCount', 'indefinite');
+  return animateElement;
 }
 
-function createMatrix() {
-    const fragment = document.createDocumentFragment();
-    const matrix = [];
-    const fontSize = getFontSize();
-    const slots = Array(Math.floor(windowHeight / fontSize)).fill('');
-    const x = getX();
-    for (let i = 0; i < slots.length; i++) {
-        const text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
-        text.setAttribute('x', x);
-        text.setAttribute('y', i * fontSize);
-        text.setAttribute('fill', color);
-        text.setAttribute('fill-opacity', fontSize/40);
-        text.textContent = getRandomCharacter();
-        text.setAttribute('font-size', `${fontSize}px`);
-        matrix[i] = text;
-        fragment.appendChild(text);
-    }
-    svg.appendChild(fragment);
-    return {
-        textToAppend: words[getWordIndex()],
-        slots,
-        matrix,
-        finish: false,
-    }
+function createWordElem(x, word) {
+  const fontSize = getFontSize();
+  const wordHeight = fontSize * word.length;
+
+  const wordContainer = createElementNS('g');
+  wordContainer.setAttribute('transform', `translate(${x}, ${Math.random() * -wordHeight})`);
+  
+  for (let i = 0; i < word.length; i++) {
+    const character = word[i];
+    const textElement = createTextElem(x, i * fontSize, character, fontSize);
+    wordContainer.appendChild(textElement);
+  }
+
+  const animateElement = createAnimationElem(x, x, -wordHeight, windowHeight);
+  wordContainer.appendChild(animateElement);
+
+  return wordContainer;
 }
 
-// Animation loop
-function animate(time) {
-    theMatrix = theMatrix.reduce((prev, val) => {
-        val.finish = !val.textToAppend && val.slots.every(val1 => !val1);
-        if (!val.finish) {
-            val.slots = shiftArray(val.slots, val.textToAppend.charAt(val.textToAppend.length - 1));
-            val.textToAppend = val.textToAppend.slice(0, -1);
-            val.matrix.forEach((text, i) => text.textContent = val.slots[i]);
-            return [...prev, val];
-        } else {
-            setTimeout(unmountSvg.bind(null, val.matrix), 100);
-            return prev;
-        }
-    }, []);
-    // theMatrix.forEach((val) => {
-    //     if (!val.finish) {
-    //         val.slots = shiftArray(val.slots, val.textToAppend.charAt(val.textToAppend.length - 1));
-    //         val.textToAppend = val.textToAppend.slice(0, -1);
-    //     } else {
-    //         garbageCollector.push([...val.matrix]);
-    //     }
-    //     val.matrix.forEach((text, i) => text.textContent = val.slots[i]);
-    // });
-    // theMatrix = theMatrix.filter(val => {
-    //     if (val.finish) {
-    //         val.matrix.forEach(text => svg.removeChild(text));
-    //     }
-    //     return !val.finish;
-    // });
-
-    requestAnimationFrame(animate);
-}
+const createNewVal = () => ({
+  x: getX(),
+  word: WORDS[getWordIndex()]
+})
 
 function pushMatrix(val) {
-    theMatrix.push(val);
-    // setTimeout(pushMatrix.bind(null, createMatrix()), getTimeout());
+  const word = createWordElem(val.x, val.word);
+  svg.appendChild(word);
 }
 
-// Start the animation
-theMatrix.push(createMatrix());
-animate();
-
-setInterval(() => {
-    pushMatrix(createMatrix());
-}, 100);
-
-
-// setTimeout(pushMatrix.bind(null, createMatrix()), getTimeout());
+for (let i = 0; i < DEFAULT_WORD_COUNT; i++) {
+  pushMatrix(createNewVal());
+}
